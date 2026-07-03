12b980982e447c0ec296358c0fe0eb1b3ec8448e
sambhavmahajan <70747596+sambhavmahajan@users.noreply.github.com>
2026-07-03 08:47:55 +0530

    Add heartbeat and add UTC to timestamps on views

--- src/main/java/com/github/sambhavmahajan/crowping/repo/ConfirmTokenRepo.java
+++ src/main/java/com/github/sambhavmahajan/crowping/repo/ConfirmTokenRepo.java
@@ -3,6 +3,7 @@ package com.github.sambhavmahajan.crowping.repo;
 import com.github.sambhavmahajan.crowping.security.ConfirmToken;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.NativeQuery;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -14,4 +15,6 @@ public interface ConfirmTokenRepo extends JpaRepository<ConfirmToken, String> {
     Optional<ConfirmToken> findByToken(String token);
     void deleteAllByExpiryDateBefore(LocalDateTime now);
     void deleteAllByVerifiedTrue();
+    @NativeQuery("select 1")
+    int ping();
 }
---
+++ src/main/java/com/github/sambhavmahajan/crowping/service/background/BackgroundHeartbeat.java
@@ -0,0 +1,29 @@
+package com.github.sambhavmahajan.crowping.service.background;
+
+import com.github.sambhavmahajan.crowping.repo.ConfirmTokenRepo;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.sql.Time;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Service
+public class BackgroundHeartbeat {
+    public final ConfirmTokenRepo repo;
+    public BackgroundHeartbeat(ConfirmTokenRepo repo) {
+        this.repo = repo;
+    }
+    @Scheduled(fixedDelay = 210000)
+    public void heartBeat() {
+        try {
+            long start = System.nanoTime();
+            repo.ping();
+            long end = System.nanoTime();
+            long tot = (end - start) / 1000000;
+            System.out.println("Heartbeat: " + tot + " milliseconds");
+        }catch(Exception ex) {
+            System.out.println("heartbeat failed");
+        }
+    }
+}
